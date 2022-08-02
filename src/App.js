0362e240bded8d1a9fda3e39eda382a2f678055c
Kevin Lawrence <36805072+klken26@users.noreply.github.com>
2022-08-02 15:00:50 +0800

    changed some code

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Load_data from './Feature_2/load_data.js'
 import FormPage from './Feature_4/form.js'
-import Button from './Feature_1/button.js'
+import Form from './Feature_1/src/Form.js'
 import HotelPage from './Feature_3/hi_modified';
 import Error from "./error.js";
 
@@ -12,7 +12,7 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/" element={<Button/>} exact/>
+          <Route path="/" element={<Form/>} exact/>
           <Route path="/search" element={<Load_data/>} />
           <Route path="/hotel" element={<HotelPage/>} />
           <Route path="/submission" element={<FormPage/>} />
